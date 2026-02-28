91d399f015b54edaf176cd49110394b6aa625b77
mochiemi <tiemi.suyama@gmail.com>
2026-02-28 15:39:09 -0300

    update: ajustes no router

--- src/router/index.ts
+++ src/router/index.ts
@@ -44,9 +44,9 @@ const router = createRouter({
       component: () => import('@/views/BlogView.vue')
     },
     {
-      path: '/blog',
-      name: 'blog',
-      component: () => import('@/views/BlogView.vue')
+      path: '/post/:slug',
+      name: 'blog-post',
+      component: () => import('@/views/BlogPostView.vue')
     },
     {
       path: '/contact',
